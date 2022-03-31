846cd761ebec6a83e9888791aa45978f6c5bdcce
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-03-31 23:44:44 +0300

    Remove create method Street, Country, City

--- Services/InvoiceAndStorage.Services.Data/Contracts/ICompanyServise.cs
+++ Services/InvoiceAndStorage.Services.Data/Contracts/ICompanyServise.cs
@@ -20,12 +20,6 @@
 
         Task<string> CreateAdress(string countryName, string cityName, string streetName, int streetNumber);
 
-        Task<string> CreateCountry(string countryName);
-
-        Task<string> CreateCity(string cityName);
-
-        Task<string> CreateStreet(string streetName, int streetNumber);
-
         Task<string> CreateCompany(AddBuyerViewModel model);
     }
 }

4ec11bd6460f87e799e0fcd0eda29b5c72bb3d53
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-01 16:58:03 +0300

    Update CreateBuyer Method

--- Services/InvoiceAndStorage.Services.Data/Contracts/IBuyerService.cs
+++ Services/InvoiceAndStorage.Services.Data/Contracts/IBuyerService.cs
@@ -2,13 +2,13 @@
 {
     using System.Collections.Generic;
     using System.Threading.Tasks;
-
+    using InvoiceAndStorage.Data.Models;
     using InvoiceAndStorage.Web.ViewModels.Buyers;
 
     public interface IBuyerService
     {
         Task<ICollection<BuyersViewModel>> All(string dbOwnerId);
 
-        Task<bool> CreateBuyer(AddBuyerViewModel model, string userId);
+        Task<bool> CreateBuyer(AddBuyerViewModel model, string userId, string dataOwner);
     }
 }

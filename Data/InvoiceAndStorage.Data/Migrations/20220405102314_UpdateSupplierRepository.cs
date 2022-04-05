eb1712c7351e68e2d3b6e2713df1034aea92ab35
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-05 21:57:33 +0300

    Update Supplier Repository

---
+++ Data/InvoiceAndStorage.Data/Migrations/20220405102314_UpdateSupplierRepository.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+
+namespace InvoiceAndStorage.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class UpdateSupplierRepository : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}

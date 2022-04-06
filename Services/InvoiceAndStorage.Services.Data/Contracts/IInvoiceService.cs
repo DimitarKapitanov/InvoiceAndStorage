cba2fb342bdb56cba7313269cc84789c2e757164
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-07 01:05:16 +0300

    Add new service

---
+++ Services/InvoiceAndStorage.Services.Data/Contracts/IInvoiceService.cs
@@ -0,0 +1,14 @@
+﻿namespace InvoiceAndStorage.Services.Data.Contracts
+{
+    using InvoiceAndStorage.Data.Models;
+    using InvoiceAndStorage.Web.ViewModels.Invoice;
+    using System.Threading.Tasks;
+
+    public interface IInvoiceService
+    {
+        InvoiceViewModel GetAllInvoiceProducts(DatabaseОwner dbOwner);
+
+        Task<bool> AddInvoice(InvoiceViewModel invoiceViewModel, DatabaseОwner dbOwner, ApplicationUser user);
+
+    }
+}

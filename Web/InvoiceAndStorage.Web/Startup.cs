fe51f17be09d75eba6700b492240085f3656053e
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-07 01:09:15 +0300

    Add IInvoiceService

--- Web/InvoiceAndStorage.Web/Startup.cs
+++ Web/InvoiceAndStorage.Web/Startup.cs
@@ -70,6 +70,7 @@
             services.AddTransient<IBuyerService, BuyerService>();
             services.AddTransient<ISupplierSevice, SupplireService>();
             services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IInvoiceService, InvoiceService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

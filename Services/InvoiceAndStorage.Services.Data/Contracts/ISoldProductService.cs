8cb8c9850b3f64fa37d0810a4129423fbbf33df3
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-09 22:09:57 +0300

    Create new service IProductService

---
+++ Services/InvoiceAndStorage.Services.Data/Contracts/ISoldProductService.cs
@@ -0,0 +1,12 @@
+﻿namespace InvoiceAndStorage.Services.Data.Contracts
+{
+    using System.Threading.Tasks;
+
+    using InvoiceAndStorage.Data.Models;
+    using InvoiceAndStorage.Web.ViewModels.Invoice;
+
+    public interface ISoldProductService
+    {
+        Task<SoldProduct> CreateSoldProduct(InvoiceProductViewModel invoiceProductViewModel, int id);
+    }
+}

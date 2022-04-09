d3975ecf5f51f54292ea2db274e8d470d192747b
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-09 22:17:52 +0300

    Update ProductViewModel

--- Web/InvoiceAndStorage.Web.ViewModels/Product/ProductViewModel.cs
+++ Web/InvoiceAndStorage.Web.ViewModels/Product/ProductViewModel.cs
@@ -1,17 +1,23 @@
 ﻿namespace InvoiceAndStorage.Web.ViewModels.Product
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
 
     public class ProductViewModel
     {
+        [Display(Name = "Име на продукта")]
         public string Name { get; set; }
 
+        [Display(Name = "Количесто на склад")]
         public int Amount { get; set; }
 
+        [Display(Name = "Единична цена")]
         public decimal Price { get; set; }
 
+        [Display(Name = "Име на доставчика")]
         public string SupplierName { get; set; }
 
+        [Display(Name = "Дата на доставка")]
         public DateTime DeliveryDate { get; set; }
     }
 }

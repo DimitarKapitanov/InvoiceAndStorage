a752f7149fc81af1614f2fd077a8bd15f444bb11
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-09 22:17:23 +0300

    Update

--- Web/InvoiceAndStorage.Web.ViewModels/Invoice/InvoiceProductViewModel.cs
+++ Web/InvoiceAndStorage.Web.ViewModels/Invoice/InvoiceProductViewModel.cs
@@ -1,15 +1,21 @@
 ﻿namespace InvoiceAndStorage.Web.ViewModels.Invoice
 {
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
     public class InvoiceProductViewModel
     {
+        [Required]
         public string ProductName { get; set; }
 
+        [Required]
+        [Range(1, int.MaxValue)]
         public int Amount { get; set; }
 
+        [Required]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
         public decimal Price { get; set; }
 
         public int Quantity { get; set; }
-
-        public decimal TotalPrice { get; set; }
     }
 }

966538c98bbb10861a816320adcd73f09aaebfc7
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-09 22:12:45 +0300

    Create new SoldProductService

---
+++ Services/InvoiceAndStorage.Services.Data/SoldProductService.cs
@@ -0,0 +1,36 @@
+﻿namespace InvoiceAndStorage.Services.Data
+{
+    using System.Threading.Tasks;
+
+    using InvoiceAndStorage.Data.Common.Repositories;
+    using InvoiceAndStorage.Data.Models;
+    using InvoiceAndStorage.Services.Data.Contracts;
+    using InvoiceAndStorage.Web.ViewModels.Invoice;
+
+    public class SoldProductService : ISoldProductService
+    {
+        private readonly IDeletableEntityRepository<SoldProduct> soldProductRepository;
+
+        public SoldProductService(IDeletableEntityRepository<SoldProduct> soldProductRepository)
+        {
+            this.soldProductRepository = soldProductRepository;
+        }
+
+        public async Task<SoldProduct> CreateSoldProduct(InvoiceProductViewModel invoiceProductViewModel, int id)
+        {
+            var soldProduct = new SoldProduct()
+            {
+                ProductName = invoiceProductViewModel.ProductName,
+                Qantity = invoiceProductViewModel.Quantity,
+                SinglePrice = invoiceProductViewModel.Price,
+                InvoiceId = id,
+                TotalValue = invoiceProductViewModel.Quantity * invoiceProductViewModel.Price,
+            };
+
+            await this.soldProductRepository.AddAsync(soldProduct);
+            await this.soldProductRepository.SaveChangesAsync();
+
+            return soldProduct;
+        }
+    }
+}

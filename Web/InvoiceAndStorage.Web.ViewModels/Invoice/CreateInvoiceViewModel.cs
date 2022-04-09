2482fdbec2ec256c464fc435440949c9ac773ab9
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-09 22:16:18 +0300

    Rename view model

---
+++ Web/InvoiceAndStorage.Web.ViewModels/Invoice/CreateInvoiceViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace InvoiceAndStorage.Web.ViewModels.Invoice
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    using InvoiceAndStorage.Data.Models.Enums;
+
+    public class CreateInvoiceViewModel
+    {
+        [Required]
+        [Display(Name ="ЕИК на клиента")]
+        public string BuyerIdentificationNumber { get; set; }
+
+        [Required]
+        [Display(Name ="Начин на плащане")]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [Required]
+        [Display(Name ="Вид на фактурата")]
+        public InvoiceTipe InvoiceTipe { get; set; }
+
+        public IList<InvoiceProductViewModel> InvoiceProductViewModels { get; set; }
+    }
+}

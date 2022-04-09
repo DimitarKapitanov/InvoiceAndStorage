faab944e31a297145b2f667e39bde72baba81dc5
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-09 22:16:31 +0300

    Create new view model

---
+++ Web/InvoiceAndStorage.Web.ViewModels/Invoice/SoldProductViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace InvoiceAndStorage.Web.ViewModels.Invoice
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using InvoiceAndStorage.Data.Models.Enums;
+
+    public class SoldProductViewModel
+    {
+        public int InvoiceNumber { get; set; }
+
+        public string BuyerName { get; set; }
+
+        public string SellerName { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime InvoiceDate { get; set; }
+
+        public PaymentMethod PaymentMethod { get; set; }
+
+        public InvoiceTipe InvoiceTipe { get; set; }
+
+        // public ICollection<Product> Products { get; set; }
+    }
+}

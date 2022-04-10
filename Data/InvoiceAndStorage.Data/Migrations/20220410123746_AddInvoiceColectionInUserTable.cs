672d2555d24eed2c0a4d96b3865757ab639477c9
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-10 23:38:33 +0300

    Add Inverse property in ApplicationUser

---
+++ Data/InvoiceAndStorage.Data/Migrations/20220410123746_AddInvoiceColectionInUserTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceAndStorage.Data.Migrations
+{
+    public partial class AddInvoiceColectionInUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

e43d09ca362bb27c5ad12ae4e61d45e80bd97343
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-11 23:12:41 +0300

    Create New Method in ISoldProductService Implement GetAllSoldProducts method

--- Services/InvoiceAndStorage.Services.Data/Contracts/ISoldProductService.cs
+++ Services/InvoiceAndStorage.Services.Data/Contracts/ISoldProductService.cs
@@ -4,9 +4,12 @@
 
     using InvoiceAndStorage.Data.Models;
     using InvoiceAndStorage.Web.ViewModels.Invoice;
+    using InvoiceAndStorage.Web.ViewModels.SoldProducts;
 
     public interface ISoldProductService
     {
         Task<SoldProduct> CreateSoldProduct(InvoiceProductViewModel invoiceProductViewModel, int id);
+
+        Task<InvoiceSoldProductsViewModel> GetAllSoldProducts(int id);
     }
 }
--- Services/InvoiceAndStorage.Services.Data/SoldProductService.cs
+++ Services/InvoiceAndStorage.Services.Data/SoldProductService.cs
@@ -1,11 +1,15 @@
 ﻿namespace InvoiceAndStorage.Services.Data
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using InvoiceAndStorage.Data.Common.Repositories;
     using InvoiceAndStorage.Data.Models;
     using InvoiceAndStorage.Services.Data.Contracts;
     using InvoiceAndStorage.Web.ViewModels.Invoice;
+    using InvoiceAndStorage.Web.ViewModels.SoldProducts;
+    using Microsoft.EntityFrameworkCore;
 
     public class SoldProductService : ISoldProductService
     {
@@ -31,6 +35,37 @@
             await this.soldProductRepository.SaveChangesAsync();
 
             return soldProduct;
+        }
+
+        public async Task<InvoiceSoldProductsViewModel> GetAllSoldProducts(int id)
+        {
+            var invoiceDetails = await this.soldProductRepository
+                .All()
+                .Where(i => i.InvoiceId == id)
+                .Include(i => i.Invoice)
+                .ThenInclude(b => b.Buyer)
+                .ThenInclude(c => c.Company)
+                .Select(x => new SoldProductsVIewModel()
+                {
+                    ProductName = x.ProductName,
+                    Quantity = x.Qantity,
+                    TotalValue = x.TotalValue,
+                    UnitPrice = x.SinglePrice,
+
+                }).OrderBy(x => x.ProductName)
+                .ToListAsync();
+
+            var allSoldProducts = new InvoiceSoldProductsViewModel();
+
+            foreach (var item in invoiceDetails)
+            {
+                allSoldProducts.SoldProducts.Add(item);
+            }
+
+            allSoldProducts.Vat = 20;
+            allSoldProducts.TotalValue = allSoldProducts.SoldProducts.Sum(x => x.TotalValue * 1.2m);
+
+            return allSoldProducts;
         }
     }
 }

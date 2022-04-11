748d56fa55c12fa735a488a08489f89e8c81f76f
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-11 23:18:13 +0300

    Create View model for SoldProductsContoller

---
+++ Web/InvoiceAndStorage.Web.ViewModels/SoldProducts/InvoiceSoldProductsViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace InvoiceAndStorage.Web.ViewModels.SoldProducts
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class InvoiceSoldProductsViewModel
+    {
+        public InvoiceSoldProductsViewModel()
+        {
+            this.SoldProducts = new List<SoldProductsVIewModel>();
+        }
+
+        [Display(Name ="ДДС")]
+        public int Vat { get; set; }
+
+        [Display(Name = "Сума с ДДС")]
+        public decimal TotalValue { get; set; }
+
+        public IList<SoldProductsVIewModel> SoldProducts { get; set; }
+    }
+}
---
+++ Web/InvoiceAndStorage.Web.ViewModels/SoldProducts/SoldProductsVIewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceAndStorage.Web.ViewModels.SoldProducts
+{
+    public class SoldProductsVIewModel
+    {
+        [Display(Name = "Име на продукта")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Брой")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Единична цена")]
+        public decimal UnitPrice { get; set; }
+
+        [Display(Name = "Общо с ДДС")]
+        public decimal TotalValue { get; set; }
+    }
+}

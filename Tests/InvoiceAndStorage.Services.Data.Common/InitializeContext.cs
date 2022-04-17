5543114e661ea1a6941bdb628eb63d5558ca0a5a
Dimitar Kapitanov <dimitarkapitanov1@abv.bg>
2022-04-17 05:18:02 +0300

    Add maper for test

---
+++ Tests/InvoiceAndStorage.Services.Data.Common/InitializeContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using InvoiceAndStorage.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceAndStorage.Services.Data.Common
+{
+    public static class InitializeContext
+    {
+        public static ApplicationDbContext CreateContextForInMemory()
+        {
+            var option = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            var context = new ApplicationDbContext(option);
+            if (context != null)
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            return context;
+        }
+    }
+}
---
+++ Tests/InvoiceAndStorage.Services.Data.Common/MapperInitializer.cs
@@ -0,0 +1,19 @@
+﻿namespace InvoiceAndStorage.Services.Data.Common
+{
+    using System.Reflection;
+    using InvoiceAndStorage.Services.Data;
+    using InvoiceAndStorage.Services.Mapping;
+    using InvoiceAndStorage.Web.ViewModels.Buyers;
+
+    public static class MapperInitializer
+    {
+        public static void InitializeMapper()
+        {
+            AutoMapperConfig.RegisterMappings(
+                typeof(CompanyService).GetTypeInfo().Assembly,
+                typeof(BuyerService).GetTypeInfo().Assembly,
+                typeof(DataBaseOwnerService).GetTypeInfo().Assembly,
+                typeof(AddBuyerViewModel).GetTypeInfo().Assembly);
+        }
+    }
+}
